18f5f6256e077b6f77e2ec0b3349ce33237481db
agurbuz <agurbuz@tatilemerhaba.com>
2018-02-28 16:39:52 +0300

    lingua bug fix

--- src/Lib/ResolveStreamContext.php
+++ src/Lib/ResolveStreamContext.php
@@ -31,23 +31,41 @@ class ResolveStreamContext {
 
         try {
 
-            //
-            $parseFile=Yaml::parse($this->app->getFile().'.yaml');
-            $realParseFile=$this->addIncludeFile($parseFile);
+            //we check for automatic files to be included.
+            //check index for stream
+            $realParseFile=$this->addIncludeFile($this->getYaml());
             $index=$this->app->getIndex();
 
+            //If the parameter is sent by the client.
+            //In this case, we do parameter checking for the stream.
             return $this->checkParamForStream($realParseFile,function() use ($realParseFile,$index) {
 
+                //if index is null
+                //all keys
                 if($index===null){
                     return $realParseFile;
                 }
+
+                //if index is not null,then return the specified key
                 return (isset($realParseFile[$index])) ? $realParseFile[$index] : null;
             });
-
 
         } catch (ParseException $e) {
             throw new \InvalidArgumentException($e->getMessage());
         }
+    }
+
+    /**
+     * @param $yamlfile null
+     * @return mixed
+     */
+    private function getYaml($yamlfile=null){
+
+        //set yaml file
+        $yamlFile=($yamlfile===null) ? $this->app->getFile() : $yamlfile;
+
+        //read the specified stream yaml
+        return Yaml::parse($yamlFile.'.yaml');
     }
 
     /**
@@ -56,14 +74,17 @@ class ResolveStreamContext {
      */
     private function addIncludeFile($parseFile){
 
-        //
+        //we take the variable of the file to be included.
         $include=$this->app->app->include;
 
+        //check include array
         if(count($include)>0){
+
+            //loop for include array
             foreach ($include as $includeValue){
 
-                //
-                $includeParseFile=Yaml::parse($this->app->app->streamHandler().'/'.$includeValue.'.yaml');
+                //parse for yaml the files will be included
+                $includeParseFile=$this->getYaml($this->app->app->streamHandler().'/'.$includeValue);
 
                 foreach ($includeParseFile as $key=>$value){
                     $parseFile[$key]=$value;
@@ -82,11 +103,13 @@ class ResolveStreamContext {
      */
     private function checkParamForStream($file,callable $callback){
 
-        //
+        //if available param array
         if(count($param=$this->app->app->param)>0){
 
             $list=[];
             foreach ($file as $key=>$value){
+
+                //check exclude for param array
                 if(!isset($param['exclude']) && in_array($key,$param)){
                     $list[$key]=$value;
                 }
@@ -100,6 +123,7 @@ class ResolveStreamContext {
             return $list;
         }
 
+        //if there is no param array
         return call_user_func($callback);
     }
 }
